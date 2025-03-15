4f31f32517c6b92f78bd3033017f78221825cea8
arden LEE <ardensdevspace@gmail.com>
2025-03-15 20:06:06 +0900

    [ADD] add header / footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="layout-body">
-        <main className="layout-main">{children}</main>
+        <main className="layout-main">
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  // 헤더에 들어갈 것: HOME / ARDEN? / BLOG
+
+  const NAVLiNK = ({ title, href }: { title: string; href: string }) => {
+    console.log(title, href, "eeeee");
+
+    return (
+      <Link href={href}>
+        <span>{title}</span>
+      </Link>
+    );
+  };
+
+  return (
+    <header className="fixed w-full flex max-w-screen-md px-4 justify-between py-5 z-50 bg-transparent">
+      <nav className="w-full">
+        <ul className="flex justify-between space-x-4">
+          <div className="space-x-4">
+            <NAVLiNK title="Home" href="/" />
+            <NAVLiNK title="Blog" href="/blog" />
+          </div>
+          <div className="ml-auto">
+            <NAVLiNK title="Arden" href="/about" />
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+}
