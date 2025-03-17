362047fd72b36174a904b38515c26378b74e957b
arden LEE <ardensdevspace@gmail.com>
2025-03-17 22:20:24 +0900

    [EDIT] 블로그 껍데기 UI 수정정

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,35 +8,28 @@ export default function BlogPage() {
   return (
     <section className="flex flex-col gap-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-4 gap-y-8">
-        <div className="flex flex-col pt-3 pb-3 hover:scale-103 hover:cursor-pointer">
-          <div className="w-[85%] mx-auto pt-3 pb-4 px-3 border border-[#C7B3FF] rounded-sm bg-[#DCDBDF]">
-            <div className="flex px-2 items-center justify-between h-6 bg-[#C7B3FF] rounded-tl-md rounded-tr-md">
-              <span className="text-white">hello...</span>
-              <div className="flex items-center space-x-1">
-                <Cat size={13} className="text-white" />
-                <Cat size={13} className="text-white" />
-                <Cat size={13} className="text-white" />
-              </div>
-            </div>
-            <Image
-              width={250}
-              height={150}
-              src=""
-              alt=""
-              className="object-cover w-full h-full max-h-[150px] md:max-h-[150px] bg-white rounded-bl-md rounded-br-md border border-white"
-            />
-            <div className="flex px-1 justify-between items-center h-8 bg-[#DCDBDF]">
-              <div className="flex">
-                <Circle size={9} className="text-black mr-1" />
-                <Circle size={9} className="text-black" />
-              </div>
-              <div className="flex items-center justify-center border-1 border-black-400 h-4 w-30">
-                <div className="w-[80%] border-1 border-black-600"></div>
-              </div>
+        <div className="border shadow-[2px_2px_2px_rgba(0,0,0,0.9)] p-[1px] hover:scale-103">
+          <div className="flex items-center justify-between px-2 pr-1 bg-[#01007A] h-6">
+            <span className="text-white font-bold text-sm">2025-03-17</span>
+            <div className="flex items-center justify-center h-4 w-4 bg-[#EBEBEB]">
+              <Cat size={18} />
             </div>
           </div>
-          <div className="w-full mx-auto border-1 border-black-400 mt-2 h-8">
-            내가 Next.js를 선택한 이유
+          <div className="flex flex-col bg-[#C0C0C0] pt-4 px-3 pb-4">
+            <div className="h-[150px] border bg-white shadow-[-2px_-2px_1px_rgba(0,0,0,1)] border-t border-l border-3 border-[#EBEBEB]">
+              사진 들어갑니다
+            </div>
+            <div className="pt-4 font-bold text-lg leading-6 line-clamp-1 break-keep">
+              타이틀 적겠습니다 타이틀 적겠습니다 타이틀 적겠습니다 타이틀
+              적겠습니다 타이틀 적겠습니다
+            </div>
+            <div className="pt-1 text-sm tracking-tight line-clamp-2">
+              블라블라 내용이 들어갑니다 내용 들어갑니다 내용 들어갑니다 내용
+              들어갑니다 내용 들어갑니다 내용 들어갑니다 내용 들어갑니다
+            </div>
+            <div className="w-full flex items-center justify-center border-b border-r border-3 border-[#EBEBEB] hover:cursor-pointer h-8 shadow-[3px_3px_2px_rgba(0,0,0,1)] mt-3">
+              play now!
+            </div>
           </div>
         </div>
       </div>
