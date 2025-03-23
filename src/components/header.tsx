12315faaa1edaa0bceb928eedd459b6c27465660
arden LEE <ardensdevspace@gmail.com>
2025-03-23 22:37:20 +0900

    Design: set header colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,6 +9,15 @@ import { Cat } from "lucide-react";
 export default function Header() {
   const icons = [1, 2, 3];
   const pathname = usePathname();
+  const styles: { [key: string]: { bg: string; text: string } } = {
+    "/": { bg: "bg-[#BFFEB8]", text: "text-[#7448ff]" },
+    "/blog": { bg: "bg-[#fdcef7]", text: "text-[#23C4AF]" },
+    "/about": { bg: "bg-[#aa9ced]", text: "text-[#ECE7D4]" },
+  };
+  const currentStyle = styles[pathname] || {
+    bg: "bg-[#bcfe4a]",
+    text: "text-[#7448FF]",
+  };
 
   const NAVLiNK = ({ title, href }: { title: string; href: string }) => {
     const isActive = pathname === href;
@@ -31,17 +40,19 @@ export default function Header() {
   return (
     <header className="fixed w-full flex max-w-screen-md z-50 bg-bgMain border-3 border-t-[var(--bgWhite)] border-r-[var(--bgWhite)] border-l-[var(--bgWhite)] border-b-0">
       <nav className="w-full border-b-3 border-b-[var(--bgSecond)]">
-        <div className="w-full flex items-center justify-between px-5 bg-bgHello h-10 border-bgSecond">
-          <span className="text-white font-bold text-sm">
+        <div
+          className={`w-full flex items-center justify-between px-5 h-10 border-bgSecond ${currentStyle.bg}`}
+        >
+          <span className={`font-bold text-md ${currentStyle.text}`}>
             Hello, arden'space!
           </span>
           <div className="flex">
             {icons.map((_, index) => (
               <div
                 key={index}
-                className="flex items-center justify-center h-6 w-6 bg-bgWhite ml-1 rounded-2"
+                className={`flex items-center justify-center h-6 w-6 ml-1 rounded-2 border-2 ${currentStyle.text}`}
               >
-                <Cat size={19} />
+                <Cat size={19} className={`${currentStyle.text}`} />
               </div>
             ))}
           </div>
