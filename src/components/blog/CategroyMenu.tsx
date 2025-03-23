b03d3b5e7df97bbc6566317d4c3af5b2f52c97b1
arden LEE <ardensdevspace@gmail.com>
2025-03-23 19:28:37 +0900

    Design: set the blog design

---
+++ src/components/blog/CategroyMenu.tsx
@@ -0,0 +1,36 @@
+import { Folder, FolderClosed } from "lucide-react";
+
+interface CategoryMenuProps {
+  categories: string[];
+  selectedCategory: string;
+  onSelectCategory: (category: string) => void;
+}
+
+export default function CategoryMenu({
+  categories,
+  selectedCategory,
+  onSelectCategory,
+}: CategoryMenuProps) {
+  return (
+    <div className="w-full grid grid-cols-4 justify-center max-w-screen-md border-t-3 border-b-3 border-[var(--bgSecond)]">
+      {categories.map((category) => (
+        <div
+          key={category}
+          className="flex items-center justify-center max-w-[200px] border-r-3 border-[var(--bgSecond)] cursor-pointer hover:bg-gray-200"
+        >
+          <div
+            className="w-[90%] flex items-center justify-center py-1 text-sm text-[var(--ttBlack)] truncate overflow-hidden"
+            onClick={() => onSelectCategory(category)}
+          >
+            {selectedCategory === category ? (
+              <FolderClosed size={15} className="mr-1" />
+            ) : (
+              <Folder size={15} className="mr-1" />
+            )}
+            {category}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,17 +3,17 @@ import { Github, LinkedinIcon, Twitter } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-auto border h-[30px]">
-      <div className="flex items-center justify-between">
+    <footer className="w-full mt-auto h-[30px] border-bgWhite bg-bgWhite">
+      <div className="w-full h-full flex items-center justify-between px-4">
         <div className="flex px-2 gap-1">
           <Link href="https://github.com/ardensgarden" target="_blank">
-            <Github size={17} />
+            <Github size={20} />
           </Link>
           <Link href="https://www.linkedin.com/in/ardenspace/" target="_blank">
-            <LinkedinIcon size={17} />
+            <LinkedinIcon size={20} />
           </Link>
         </div>
-        <span className="text-sm">
+        <span className="text-xs">
           Copyright © {new Date().getFullYear()} Arden Lee
         </span>
       </div>
