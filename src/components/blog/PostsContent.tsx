c6142e610ce10016987d1b9934367397b8e1c25e
arden LEE <ardensdevspace@gmail.com>
2025-03-25 21:44:25 +0900

    Design: complete the post box design

--- src/components/blog/PostsContent.tsx
+++ src/components/blog/PostsContent.tsx
@@ -1,37 +1,47 @@
 import { CategoryPost } from "@/app/blog/page";
-import { Diameter, Circle, Cat, Heart } from "lucide-react";
+import Image from "next/image";
+import { Dog } from "lucide-react";
 
 interface PostContentProps {
   posts?: CategoryPost[];
 }
 
 const PostsContent: React.FC<PostContentProps> = ({ posts = [] }) => {
-  console.log(posts, "????????????????");
   return (
-    <section className="flex flex-col mt-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-5 px-5">
+    <section className="flex flex-col pt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-4 gap-8 px-1 sm:px-4">
         {posts.map((post) => (
           <div
             key={post.title}
-            className="border shadow-[2px_2px_2px_rgba(0,0,0,0.9)] p-[1px] hover:scale-103"
+            className="shadow-[3px_3px_3px_rgba(0,0,0,0.2)] p-[2px] hover:scale-102 rounded-sm hover:cursor-pointer"
           >
-            <div className="flex items-center justify-between px-2 pr-1 bg-[#01007A] h-6">
+            <div className="flex items-center justify-between px-2 pr-1 bg-[#01027a] h-6">
               <span className="text-white font-bold text-sm">{post.date}</span>
-              <div className="flex items-center justify-center h-4 w-4 bg-[#EBEBEB]">
-                <Cat size={18} />
+              <div className="flex items-center justify-center h-4 w-4 bg-bgWhite">
+                <Dog size={18} />
               </div>
             </div>
-            <div className="flex flex-col bg-[#C0C0C0] pt-4 px-3 pb-4">
-              <div className="h-[150px] border bg-white shadow-[-2px_-2px_1px_rgba(0,0,0,1)] border-t border-l border-3 border-[#EBEBEB]">
-                사진 들어갑니다
+            <div className="flex flex-col bg-[#fdecf7] pt-4 px-3 pb-4">
+              <div className="h-[200px] border-t border-l border-3 border-[rgba(35,196,175,0.7)]">
+                <Image
+                  unoptimized
+                  width={300}
+                  height={200}
+                  src={post.thumbnail}
+                  alt={post.title}
+                  className="object-cover w-full h-full"
+                />
               </div>
-              <div className="pt-4 font-bold text-lg leading-6 line-clamp-1 break-keep">
+              <div className="pt-5 font-bold text-lg leading-6 line-clamp-1 break-keep">
                 {post.title}
               </div>
-              <div className="pt-1 text-sm tracking-tight line-clamp-2">
+              <div
+                className="pt-1 text-sm tracking-tight line-clamp-2"
+                style={{ minHeight: "3.3em" }}
+              >
                 {post.description}
               </div>
-              <div className="w-full flex items-center justify-center border-b border-r border-3 border-[#EBEBEB] hover:cursor-pointer h-8 shadow-[3px_3px_2px_rgba(0,0,0,1)] mt-3">
+              <div className="w-full flex items-center justify-center border-b border-r border-1 border-[rgba(0,0,0,0.4)] h-8 shadow-[3px_3px_2px_rgba(35,196,175,0.7)] mt-3 rounded-xs">
                 play now!
               </div>
             </div>
