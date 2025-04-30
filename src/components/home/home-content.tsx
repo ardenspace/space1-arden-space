36e9db0894891bef4cc241fe8f5774a1f09355ed
arden LEE <ardensdevspace@gmail.com>
2025-04-30 23:03:46 +0900

    Design: 블로그 화면 얼추 구상

--- src/components/home/home-content.tsx
+++ src/components/home/home-content.tsx
@@ -7,12 +7,15 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { homeIcons } from "./home-icons";
 import HomeIcon from "./HomeIcon";
+import { useWindowSize } from "@/hooks/use-size";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function HomeContent() {
   // 헤더 - theme changer,
   const myLocale = useLocale();
+  const size = useWindowSize();
+  if (!size) return null;
 
   return (
     <section className="w-full h-full relative">
@@ -34,13 +37,25 @@ function HomeContent() {
 
       {/* blog contents */}
       <div className="flex flex-col h-full justify-center items-center relative">
-        <div className="h-[45vh] min-h-[300px] w-[70vw] min-w-[300px] max-w-[570px] border-2 border-[#fff] bg-[var(--footerBg)]">
-          1 blog
+        <div className="h-[45vh] min-h-[300px] w-[60%] min-w-[320px] max-w-[600px] border-2 border-[var(--bgWhite)] bg-[var(--footerBg)] mt-[10vh] z-9 flex flex-col items-center">
+          <div className="h-[6%] w-[99%] border flex items-center justify-center text-sm">
+            HELLO, ARDEN'SPACE!
+          </div>
+          <div className="border h-[88%] w-[99%]">
+            <div>이건 내용</div>
+          </div>
+          <div className="h-[6%] w-[99%] border"></div>
+          {/* <div>HELLO, ARDEN'SPACE!</div>
+          <div>
+            <div>welcome to arden'space</div>
+            <div>최신 게시글</div>
+            <div>현재 진행 중인 프로젝트 + 프로그래스 바</div>
+          </div> */}
         </div>
 
         {/* portfolio */}
         <div className="absolute top-[15vh] left-[100px] h-[25vh] min-h-[200px] w-[40vw] min-w-[320px] max-w-[360px] border-2 border-[#fff] transition-transform duration-200 hover:scale-103">
-          <div className="h-[10%] bg-[var(--ttBlack)] text-[var(--pfColor)] flex items-center px-2">
+          <div className="h-[10%] bg-[var(--ttBlack)] text-[var(--pfColor)] flex items-center px-2 border-b-2 border-[#fff]">
             WHO IS THIS?
           </div>
           <div className="checkerboard">
