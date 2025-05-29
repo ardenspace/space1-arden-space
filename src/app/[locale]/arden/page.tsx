df8a6c5dc4ee9a16279261b2b3464b595bb5caba
arden LEE <ardensdevspace@gmail.com>
2025-05-29 22:34:13 +0900

    [DOCS] 포폴에 단어 추가가

--- src/app/[locale]/arden/page.tsx
+++ src/app/[locale]/arden/page.tsx
@@ -10,8 +10,11 @@ export default function AboutPage() {
       <section>
         <div>arden? page 입니다</div>
         <div>내 기술</div>
+        <div>React Native, React.js, Next.js, TypeScript, Tailwind</div>
         <div>내 장점</div>
         <div>컴포넌트화 / 재사용성 고려</div>
+        <div>사용자 편의성? 끈질김, 집요함</div>
+        <div></div>
       </section>
     </PageLayout>
   );
