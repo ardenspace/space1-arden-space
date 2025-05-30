3b4e72ed62ae0546e4502c22b59e66d41c9bba67
arden LEE <ardensdevspace@gmail.com>
2025-05-31 00:09:58 +0900

    [Design] 디자인 정리 및 프로필 사진 정리

--- src/components/home/ArdensBlog.tsx
+++ src/components/home/ArdensBlog.tsx
@@ -24,26 +24,30 @@ export default function ArdensBlog() {
       <div className="h-[91.5%] w-full">
         <div className="flex flex-wrap w-full h-full pt-[5px] pb-[5px] [container-type:size]">
           <div className="min-w-[250px] flex-1 px-2">
-            <span className="font-bold text-base">What I've done?</span>
-            <div className="flex mt-[5px] h-[70%] [@container(min-width:500px)]:flex-col border-2 border-dotted border-[var(--ttBlack)]">
-              <div className="flex-[1.8] relative">
-                {/* <img
-                  src="/thumbnails/test1.jpg"
+            <span className="text-[var(--ttBlack)] font-bold text-base">
+              What I've done?
+            </span>
+            <div className="custom-box flex mt-[5px] h-[90%] [@container(min-width:500px)]:flex-col border-2 border-dotted border-[var(--ttBlack)]">
+              <div className="flex-[1] relative">
+                <img
+                  src="/thumbnails/nextjs-logo.png"
                   alt="attie-on-the-home"
                   className="saturate-50 absolute inset-0 w-full h-full object-cover"
-                /> */}
+                />
               </div>
               {/* #BFBFBF */}
               <div
                 className="
-            flex-[0.2] flex items-center text-base font-bold text-[var(--mainTt3)] p-2 bg-[var(--footerBr)] cursor-pointer 
+            flex-[0.3] flex items-center text-base font-bold text-[var(--mainTt2)] p-2 bg-[var(--footerBg)] cursor-pointer 
             border-dotted border-[var(--ttBlack)]
             [@container(min-width:500px)]:border-t-2 
             [@container(max-width:500px)]:border-l-2
           "
                 onClick={onClickTakeAPeek}
               >
-                <span className="bounce-up">{">>"} Enter the vault !</span>
+                <span className="text-xl bounce-up">
+                  {">>"} Enter the vault
+                </span>
               </div>
             </div>
           </div>
@@ -57,14 +61,17 @@ export default function ArdensBlog() {
               />
             </div>
 
-            <span className="font-bold text-base">What I’m up to?</span>
+            <span className="text-[var(--ttBlack)] font-bold text-base">
+              What I’m up to?
+            </span>
             <div className="h-[65%] flex flex-col mt-[5px]">
               <ProgressBar />
               <button
                 className="mt-[10px] h-full flex items-center justify-center border-b border-r border-2 border-[var(--bgWhite)] shadow-[2px_2px_1px_var(--mainTt2)] cursor-pointer"
                 onClick={onClickVaultButton}
               >
-                <span className="text-[var(--mainTt3)] text-[1.2rem] [@container(max-width:500px)]:text-[1rem] bounce-right">
+                {/* bounce-right */}
+                <span className="text-[var(--mainTt3)] text-[1.2rem] [@container(max-width:500px)]:text-[1rem]">
                   ▶▶
                 </span>
               </button>
--- src/components/home/WhoIsArden.tsx
+++ src/components/home/WhoIsArden.tsx
@@ -9,7 +9,7 @@ export default function WhoIsArden({ locale }: { locale: string }) {
       onClick={() => setCurrentPosition("port")}
       className={`${
         currentPosition === "blog" ? "" : "z-9"
-      } absolute top-[15vh] left-[100px] h-[25vh] min-h-[220px] w-[40vw] min-w-[320px] max-w-[360px] border-2 border-[#fff] transition-transform duration-200 hover:scale-103 `}
+      } absolute top-[14vh] left-[100px] h-[25vh] min-h-[220px] w-[40vw] min-w-[320px] max-w-[360px] border-2 border-[#fff] transition-transform duration-200 hover:scale-103 `}
       style={{
         boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
       }}
@@ -21,7 +21,7 @@ export default function WhoIsArden({ locale }: { locale: string }) {
         <div className="w-[85%] h-[85%] bg-white relative overflow-hidden rotate-[8deg] cursor-pointer">
           <Link href={`http://localhost:3000/${locale}/arden`}>
             <img
-              src="/home/wemadeit.jpg"
+              src="/home/profile2.jpeg"
               alt="for-home-portfolio"
               className="absolute inset-0 w-full h-full object-cover"
             />
