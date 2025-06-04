6de7bd073f29f7820e22e7d018f498c17d279f35
arden LEE <ardensdevspace@gmail.com>
2025-06-04 21:30:53 +0900

    [Docs] 조금씩 조금씩 전진 . . .

--- src/app/[locale]/arden/page.tsx
+++ src/app/[locale]/arden/page.tsx
@@ -14,7 +14,13 @@ export default function AboutPage() {
         <div>내 장점</div>
         <div>컴포넌트화 / 재사용성 고려</div>
         <div>사용자 편의성? 끈질김, 집요함</div>
-        <div></div>
+        <div>2023. 04. 17 ~</div>
+        <div>(주) 쿳션</div>
+        <div>부천 리빙랩 관리자 페이지</div>
+        <div>FPP 관리자 페이지 서버와 프론트 둘 다 담당</div>
+        <div>곰쿡 관리자 페이지</div>
+        <div>휴먼앤로봇 제조현황 및 픽업 앱</div>
+        <div>아이스크림 로봇 주문 앱</div>
       </section>
     </PageLayout>
   );
