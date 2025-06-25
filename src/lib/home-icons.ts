3eafc81f79853dc20c2d7d7d712874da2edbd0e1
arden LEE <ardensdevspace@gmail.com>
2025-06-25 23:07:55 +0900

    [CHORE] 필요 없는 거 삭제

--- src/lib/home-icons.ts
+++ src/lib/home-icons.ts
@@ -1,25 +1,7 @@
 // icons.ts
-import {
-  Telescope,
-  Satellite,
-  Twitter,
-  Github,
-  LinkedinIcon,
-} from "lucide-react";
+import { Twitter, Github, LinkedinIcon } from "lucide-react";
 
 export const homeIcons = [
-  // {
-  //   ICON: Telescope,
-  //   design: "top-[20px] left-[10px]",
-  //   filename: "BLOG",
-  //   tolink: `/blog`,
-  // },
-  // {
-  //   ICON: Satellite,
-  //   design: "top-[20px] left-[90px]",
-  //   filename: "ARDEN?",
-  //   tolink: `/about`,
-  // },
   {
     ICON: Twitter,
     design: "bottom-[1vh] right-[55px]",
