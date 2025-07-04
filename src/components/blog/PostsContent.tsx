fd6830563db2d4a36a8c9703212047fc11bd97f7
arden LEE <ardensdevspace@gmail.com>
2025-07-04 23:35:34 +0900

    [DESIGN] 포스트 및 홈화면 전면 수정

--- src/components/blog/PostsContent.tsx
+++ src/components/blog/PostsContent.tsx
@@ -4,7 +4,7 @@ import type { Post } from "@/app/[locale]/blog/ClientBlogPage";
 import Link from "next/link";
 import { useLocale } from "@/hooks/use-locale";
 import Image from "next/image";
-import { Dog } from "lucide-react";
+import { Cat } from "lucide-react";
 
 interface PostContentProps {
   posts?: Post[];
@@ -14,25 +14,23 @@ const PostsContent: React.FC<PostContentProps> = ({ posts = [] }) => {
 
   return (
     <section className="flex flex-col pt-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-x-4 gap-8 px-1 sm:px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-2 gap-8 px-1 sm:px-4">
         {posts.map((post, idx) => (
           <div
             key={idx}
-            className="shadow-[2px_2px_2px_rgba(0,0,0,0.2)] p-[2px] hover:scale-102 rounded-sm hover:cursor-pointer"
+            className="shadow-[2px_2px_1px_var(--bgSecond)] p-[2px] hover:scale-103 rounded-sm hover:cursor-pointer"
           >
             <Link
               href={`/${locale}/blog/${post.category}/${post.slug}?fc=${post.category}`}
             >
-              <div className="flex items-center justify-between px-2 pr-1 bg-[var(--postHd)] h-6">
-                <span className="text-white font-bold text-sm">
-                  {post.date}
-                </span>
-                <div className="flex items-center justify-center h-4 w-4 bg-bgWhite">
-                  <Dog size={18} />
+              <div className="flex items-center justify-between px-2 pr-1 h-6 mb-1 bg-[#e8e8e8] border-1 border-[var(--bgSecond)] rounded-xs">
+                <span className="text-sm text-ttBlack">{post.date}</span>
+                <div className="flex items-center justify-center text-ttBlack">
+                  <Cat size={18} />
                 </div>
               </div>
-              <div className="flex flex-col bg-[#ffedf9] pt-2 px-1 pb-4">
-                <div className="flex justify-center h-[170px] border-t border-l border-3 border-[var(--bgWhite)] shadow-[-2px_-2px_1px_rgba(35,196,175,0.7)]">
+              <div className="flex flex-col pt-2 pb-4 px-1 bg-[#e8e8e8] border-1 border-[var(--bgSecond)] rounded-xs">
+                <div className="flex justify-center h-[200px] border-t border-l border-3 border-[var(--bgWhite)] shadow-[-2px_-2px_1px_var(--bgSecond)]">
                   <Image
                     unoptimized
                     width={270}
@@ -42,18 +40,17 @@ const PostsContent: React.FC<PostContentProps> = ({ posts = [] }) => {
                     className="object-cover w-full h-full bg-[var(--bgWhite)]"
                   />
                 </div>
-                <div className="px-3">
-                  <div className="pt-5 font-bold text-lg leading-6 line-clamp-1 break-keep">
+                <div className="px-1">
+                  <div className="pt-4 font-bold text-base leading-6 line-clamp-1 break-keep">
                     {post.title}
                   </div>
                   <div
-                    className="pt-1 text-sm tracking-tight line-clamp-1"
-                    // style={{ minHeight: "3.3em" }}
-                    style={{ minHeight: "2em" }}
+                    className="pt-1 text-sm tracking-tight line-clamp-2"
+                    style={{ minHeight: "3em" }}
                   >
                     {post.description}
                   </div>
-                  <div className="w-full flex items-center justify-center border-b border-r border-3 border-[var(--bgWhite)] h-8 shadow-[2px_2px_1px_rgba(35,196,175,0.7)] mt-3 rounded-xs tracking-[0.5em]">
+                  <div className="shadow-out-button mt-3 tracking-[0.6em] text-base">
                     play now!
                   </div>
                 </div>
