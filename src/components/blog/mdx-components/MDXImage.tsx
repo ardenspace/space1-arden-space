331787742c36a2f56cdd24071bd045d2ff02796d
arden LEE <ardensdevspace@gmail.com>
2025-09-19 06:20:13 +0900

    design: 글 내 이미지 반응형 처리 (480px 이하면 이미지 100% 처리)

--- src/components/blog/mdx-components/MDXImage.tsx
+++ src/components/blog/mdx-components/MDXImage.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useWindowSize } from "@/hooks/use-size";
 import Image, { ImageProps } from "next/image";
 
 export default function MDXImage(props: ImageProps) {
+  const { width } = useWindowSize();
+
+  // 480px 이하면 이미지 width 100%, 이상이면 style로 넘어온 값 사용
+  const isSmall = width > 0 && width < 480;
+
   return (
     <div className="flex justify-center my-3">
       <Image
@@ -10,9 +16,9 @@ export default function MDXImage(props: ImageProps) {
         width={props.width ?? 800}
         height={props.height ?? 600}
         style={{
-          width: "100%",
-          height: "auto",
           ...props.style,
+          width: isSmall ? "100%" : props.style?.width || "100%",
+          height: "auto",
         }}
       />
     </div>
