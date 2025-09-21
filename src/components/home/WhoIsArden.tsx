bf57d293bff38dddabfe5f63a6579e83a0c7e816
arden LEE <ardensdevspace@gmail.com>
2025-09-21 17:37:37 +0900

    feat: whoisarden에 DraggableWindow 적용

--- src/components/home/WhoIsArden.tsx
+++ src/components/home/WhoIsArden.tsx
@@ -1,23 +1,26 @@
 import { useZIndex } from "@/contexts/ZIndexContext";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import DraggableWindow from "./DraggableWindow";
 
 export default function WhoIsArden({ locale }: { locale: string }) {
   const { currentPosition, setCurrentPosition } = useZIndex();
 
   return (
-    <div
-      onClick={() => setCurrentPosition("port")}
+    <DraggableWindow
+      initialPosition={{ x: 100, y: 130 }}
       className={`${
         currentPosition === "blog" ? "" : "z-9"
-      } absolute top-[14vh] left-[100px] h-[20vh] min-h-[200px] max-h-[220px] w-[40vw] min-w-[300px] max-w-[320px] border-2 border-[#fff] transition-transform duration-200 hover:scale-103`}
+      } h-[20vh] min-h-[200px] max-h-[220px] w-[40vw] min-w-[300px] max-w-[320px] border-2 border-[#fff] transition-transform duration-200 hover:scale-103`}
       style={{
         boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.5)",
       }}
+      onDragStart={() => setCurrentPosition("port")}
     >
       <div className="h-[10%] bg-[var(--ttBlack)] text-[var(--portTt)] flex items-center px-2 border-b-2 border-[#fff] font-bold text-xs">
         WHO IS ARDEN?
       </div>
+
       <div className="checkerboard">
         <div className="w-[90%] h-[90%] bg-white relative overflow-hidden rotate-[4deg] cursor-pointer">
           <Link href={`/${locale}/arden`}>
@@ -31,6 +34,6 @@ export default function WhoIsArden({ locale }: { locale: string }) {
           </Link>
         </div>
       </div>
-    </div>
+    </DraggableWindow>
   );
 }
